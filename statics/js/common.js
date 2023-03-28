3fdef5ef1802d211da93b646bd91298d3dbbc3d9
kksuin <wlgus828@naver.com>
2023-03-28 22:10:46 +0900

    메인 html/css + 토글 이벤트 추가

---
+++ statics/js/common.js
@@ -0,0 +1,11 @@
+function moveSlide(_this,targetData){
+    let _parent = $(_this).closest('.slide-button-list')
+    let targetGorup = _parent.siblings('.slide-content-list')
+    let target = '.slide__item[data-slide="'+targetData+'"]'
+    let activeClass = '.is-active'
+
+    // targetGorup.find(target).addClass(activeClass).siblings().removeClass(activeClass)
+    targetGorup.find(target).show().siblings().hide()
+    
+    console.log(target)
+}
